e5ca1118a27307b2b390f79760f97dd2e8d00a77
Antoine <antoinebdu85@gmail.com>
2023-08-13 20:27:39 +0200

    Added more stats from singleplayer

--- RDR_Stats.c
+++ RDR_Stats.c
@@ -1,12 +1,16 @@
-//List of stats - Counted 695 stats - Only tried in Multiplayer - Im Foxxyyy
+//List of RDR1 stats used in the script files, there seems to be 696 - Im Foxxyyy
 
-//Not sure
+//Unsure
 Money //STAT 0
 Honor //STAT 1
 Fame //STAT 3
 Bounty //STAT 222
 
 //Combat
+Duels won //STAT 451
+Duel challenges turned down //STAT 453
+Duels won by disarm //454
+Duel hats shot off //STAT 455
 Animals killed //STAT 26
 People killed //STAT 27
 Unaffiliated criminals killed //STAT 34
@@ -17,6 +21,66 @@ Banditos killed //STAT 38
 Rebels killed //STAT 39
 Dutch's gang killed //STAT 40
 Enemy horses killed //STAT 41
+Volcanic Pistol shots hit //STAT 50
+Semi-automatic Pistol shots hit //STAT 51
+High Power Pistol shots hit //STAT 52
+Mauser Pistol shots hit //STAT 53
+Cattleman revolver shots hit //STAT 54
+Schofield revolver shots hit //STAT 55
+Double-action revolver shots hit //STAT 56
+Lemat revolver shots hit //STAT 57
+Repeater Carbine shots hit //STAT 58
+Winchester repeater shots hit //STAT 59
+Henry repeater shots hit //STAT 60
+Evans repeater shots hit //STAT 61
+Springfield rifle shots hit //STAT 62
+Bolt-action rifle shots hit //STAT 63
+Buffalo rifle shots hit //STAT 64
+Sawed-off shotgun shots hit //STAT 65
+Double-barreled shotgun shots hit //STAT 66
+Pump-action shotgun shots hit //STAT 67
+Semi-automatic shotgun shots hit //STAT 68
+Rolling block rifle shots hit //STAT 69
+Carcano rifle shots hit //STAT 70
+Explosive Rifle shots hi //STAT 81
+Lasso successes //STAT 71
+Knife hits //STAT 72
+Fire bottle hits //STAT 73
+Dynamite hits //STAT 74
+Throwing Knife hits //STAT 75
+Tomahawk hits //STAT 79
+Gatling gun shots hit //STAT 76
+Browning gun shots hit //STAT 77
+Cannon shots hit //STAT 78
+Victims run over //STAT 129
+Victims run over wit horse //STAT 130
+Victims run over with cart //STAT 131
+Victims run over with wagon //STAT 132
+Victims run over with stagecoach //STAT 133
+Shots fired while crouching //STAT 134
+Shots fired from cover //STAT 135
+Shots fired from horseback //STAT 136
+Shots fired from vehicle //STAT 137
+Animals killed while riding train //STAT 138
+Poeple killed while riding train //STAT 139
+People killed with Volcanic Pistol //STAT 183
+People killed with Semi-automatic Pistol //STAT 184
+People killed with High Power Pistol //STAT 185
+People killed with Mauser Psitol //STAT 186
+People killed with Cattleman revolver //STAT 187
+People killed with Schofield revolver //STAT 188
+People killed with Double-action revolver //STAT 189
+People killed with Lemat revolver //STAT 190
+People killed with Repeater carbine //STAT 191
+People killed with Winchester repeater //STAT 192
+People killed with Henry repeater //STAT 193
+People killed with Evans repeater //STAT 194
+People killed with Springfield rifle //STAT 195
+People killed with Bolt-action rifle //STAT 196
+People killed with Buffalo rifle //STAT 197
+People killed with Sawed-off shotgun //STAT 198
+People killed with Double-barreled shotgun //STAT 199
+People killed with Pump-action shotgun //STAT 200
 People killed with Semi-automatic shotgun //STAT 201
 People killed with Rolling block rifle //STAT 202
 People killed with Carcano rifle //STAT 203
@@ -51,7 +115,8 @@ Pump-action shotgun headshots //STAT 157
 Semi-automatic shotgun headshots //STAT 158
 Rolling block rifle headshots //STAT 159
 Carcano rifle headshots //STAT 160
-Explosive Rifle headshots //STAT 170
+Explosive Rifle headshots //STAT 170 (Multiplayer?)
+Explosive Rifle headshots //STAT 171
 Lasso headshots //STAT 161
 Knife headshots //STAT 162
 Fire bottle headshots //STAT 163
@@ -60,15 +125,26 @@ Throwing Knife headshots //STAT 165
 Tomahawk headshots //STAT 169
 Gatling gun headshots //STAT 166
 Browning gun headshots //STAT 167
-Cannon headshots //STAT 162
+Cannon headshots //STAT 162 (Multiplayer?)
+Cannon headshots //STAT 168
 Knockouts //STAT 43
 Headshots //STAT 42
 Hatshots //STAT 45
 Disarms //STAT 46
 Longest headshot streak //STAT 47
 Longest disarm streak //STAT 48
+Passersby attacked //STAT 367
+Passersby defended //STAT 368
 
 //Crimes
+Active bounty total //STAT 222
+US bounty //STAT 223
+Mexico bounty //STAT 224
+Time in jail //STAT 225
+Counts of vandalism //STAT 226
+Counts of vehicle vandalism //STAT 227
+Counts of property vandalism //STAT 228
+Counts of vehicle destruction //STAT 229
 Counts of arson //STAT 230
 Counts of assault //STAT 231
 Counts of law officer assault //STAT 232
@@ -82,11 +158,43 @@ Counts of livestock slaughter //STAT 239
 Counts of animal slaughter //STAT 240
 Counts of horse theft //STAT 241
 Counts of vehicle theft //STAT 242
+Counts of abduction //STAT 243
+Counts of law officer abduction //STAT 244
+Counts of safe cracking //STAT 245
+Counts of trespassing //STAT 247
+Counts of robbery //STAT 248
+Counts of law officer threatening //STAT 250
 Biggest bounty //STAT 252
+Bribes paid //STAT 253
+Biggest bounty paid //STAT 254
+Biggest bribe paid //STAT 255
+Biggest bounty pardonned //STAT 256
+Posses sent after you //STAT 257
+Longest pursuit by local sheriffs //STAT 258
+Longest pursuit by marshals //STAT 259
+Times most wanted //STAT 260
+Crimes commited during marshal pursuit //STAT 261
+Longest kidnapping streak //STAT 262
+Percent of times sided with bandits //STAT 263
 Times Public Enemy //STAT 386
+
+//Travels
+Total spent on stagecoach rides //STAT 384
+Travelers escorted //STAT 369
+Transports completed //STAT 366
+
+//Money
+Poker winnings //STAT 411
+Liar's dice winings //STAT 429
+Horseshoes winnings //STAT 433
+Five finger fillet winnings //STAT 437
+Arm wrestling winnings //STAT 441
+Blackjack winnings //STAT 445
+Total gambling winnings //STAT 450
 
 //Collectibles
 Values of herbs collected //STAT 273
+Total pounds of meat harvested //STAT 348
 Butterfly Weed //STAT 265
 Desert Sage //STAT 264
 Golden Currant //STAT 267
@@ -97,9 +205,60 @@ Red Sage //STAT 271
 Violet Snowdrop //STAT 270
 Wooly Blue Curls //STAT 268
 Wild Feverfew //STAT 272
+Armadillos skinned //STAT 274
+Bats skinned //STAT 275
+Bears skinned //STAT 276
+Beavers skinned //STAT 277
+Bighorn skinned //STAT 278
+Boars skinned //STAT 279
+Bobcats skinned //STAT 280
+Buck skinned //STAT 281
+Buffalo skinned //STAT 282
+Chicken feathers //STAT 283
+Cougars skinned //STAT 284
+Coyotes skinned //STAT 286
+Cows skinned //STAT 285
+Crow feathers //STAT 287
+Deer skinned //STAT 288
+Dogs skinned //STAT 289
+Duck feathers //STAT 290
+Eagle feathers //STAT 291
+Elk skinned //STAT 292
+Foxes skinned //STAT 293
+Goats skinned //STAT 294
+Hawk feathers //STAT 295
+Horses skinned //STAT 312
+Mules skinned //STAT 297
+Owl feathers //STAT 302
+Pigs skinned //STAT 303
+Rabbits skinned //STAT 304
+Raccoons skinned //STAT 305
+Seagull feathers //STAT 295
+Sheep skinned //STAT 306
+Skunks skinned //STAT 307
+Snakes skinned //STAT 308
+Songbird feathers //STAT 309
+Vulture feathers //STAT 310
+Wolves skinned //STAT 311
+Jackalopes skinned //STAT 598
+Treasures collected //STAT 357
 
 //General
 Total time played //STAT 264 (unsure)
+Missions completed //STAT 9
+Missions cancelled //STAT 10
+Missions retried //STAT 11
+New Austin missions //STAT 12
+Nuevo Paraiso missions //STAT 13
+West Elizabeth missions //STAT 14
+Homestead missions //STAT 15
+Ambushes survived //STAT 16
+Trips skipped //STAT 17
+Areas discovered //STAT 473
+New Austin areas discovered //STAT 474
+Nuevo Paraiso areas discovered //STAT 475
+West Elizabeth areas discovered //STAT 476
+Game completed //STAT 59
 Gang hideouts cleared //STAT 458
 Twin Rocks hideout fastest clear time //STAT 459
 Solomon's Folly hideout fastest clear time //STAT 460
@@ -109,9 +268,80 @@ Gaptooth Breach hideout fastest clear time //STAT 463
 Fort Mercer hideout fastest clear time //STAT 464
 Tesoro Azul hideout fastest clear time //STAT 465
 Nosalida hideout fastest clear time //STAT 463
+Player houses //STAT 468
 Rare Weapons //STAT 469
+Job locations completed //STAT 400
+Favorite Job //STAT 401
+Nightwatch jobs completed //STAT 402
+Horse breaking jobs completed //STAT 403
+Favorite job location //STAT 404
+Horses broken //STAT 405
+Strangers met //STAT 406
+Tasks completed for strangers //STAT 407
+World events encountered //STAT 408
+Unique world events encounetered //STAT 409
+Victims hogtied //STAT 398
+Hogtie victims freed //STAT 399
+
+//Score
+Different minigames won //STAT 410
+Poker games lost //STAT 412
+Biggest poker pot won //STAT 413
+Poker hands won with high card //STAT 414
+Poker hands won with pair //STAT 415
+Poker hands won with two pair //STAT 416
+Poker hands won with three of a kind //STAT 417
+Poker hands won with straight //STAT 418
+Poker hands won with full house //STAT 419
+Poker hands won with flush //STAT 420
+Poker hands won with four of a kind //STAT 421
+Poker hands won with straight flush //STAT 422
+Poker hands won with royal flush //STAT 423
+Poker hands won with a bluff //STAT 424
+Times caught cheating //STAT 426
+Times gotten away with cheating //STAT 427
+Times spent playing poker //STAT 425
+Liar's dice games lost //STAT 430
+Liar's dice wins without losing a die //STAT 431
+Time spent playing liar's dice //STAT 432
+Total ringers //STAT 434
+Most ringers in one horseshoes game //STAT 435
+Time spent playing horseshoes //STAT 436
+Five finger fillet games lost //STAT 438
+Longest five finger fillet win streak //STAT 439
+Time spent playing five finger fillet //STAT 440
+Arm wrestling matches lost //STAT 443
+Longest arm wrestling match //STAT 444
+Time spent playing arm wretsling //STAT 442
+Blackjack hands lost //STAT 447
+Most won in single blackjack session //STAT 446
+Blackjack hands played //STAT 448
+Total drinks consumed //STAT 449
+Fastest safe cracking //STAT 251
+Cows herded //STAT 355
+Horses herded //STAT 356
+Bounties alive //STAT 369
+Bounties Dead //STAT 358
+Bounty hunter - New Austin //STAT 360
+Bounty hunter - Nuevo Paraiso //STAT 361
+Bounty hunter - West Elizabeth //STAT 362
+Bounty hunter - New Austin locations //STAT 363
+Bounty hunter - Nuevo Paraiso locaions //STAT 364
+Bounty hunter - West Elizabeth locations //STAT 365
 
 //Misc
+Chests opened //STAT 467
+Time using binoculars //STAT 471
+Snake oil used //STAT 477
+Moonshine used //STAT 478
+Chewing tobacco used //STAT 479
+Medicine used //STAT 480
+Bait used //STAT 481
+Horse pills used //STAT 482
+Apples used //STAT 483
+Pardon letters used //STAT 484
+Vittles used //STAT 485
+Tonic used //STAT 486
 Armadillos killed //STAT 313
 Bats killed //STAT 314
 Bears killed //STAT 315
@@ -153,10 +383,13 @@ Social Club animal 3s killed //STAT 351
 Social Club animal 4s killed //STAT 352
 Social Club animal 5s killed //STAT 353
 Wild animal species killed //STAT 354
+Outfits discovered //STAT 456
+Outfits completed //STAT 457
 Multiplayer experience //STAT 487
 Kill Chain //STAT 488
 
-//Unsafe code to get/set stats values - should be simplified 
+//Unsafe code to get the value of a game stat
+//Equivalent to return Global_34581[statIndex];
 unsafe float GetStatValue(int statIndex)
 {
 	__getFrame(0);
@@ -164,6 +397,9 @@ unsafe float GetStatValue(int statIndex)
 	__getArray(1);
 	return __popF();
 }
+
+//Unsafe code to set the value of a game stat
+//Equivalent to Global_34581[statIndex] = TO_FLOAT(statValue);
 unsafe void SetStatValue(int statIndex, int statValue)
 {
 	__getFrame(1);
@@ -172,26 +408,3 @@ unsafe void SetStatValue(int statIndex, int statValue)
 	__getGlobalP(34581);
 	__setArray(1);
 }
-
-//Didn't try
-Global_34581[372] = UNK_0x5545C218(0);
-Global_34581[373] = UNK_0x5545C218(1);
-Global_34581[374] = UNK_0x5545C218(2);
-Global_34581[375] = UNK_0x5545C218(3);
-Global_34581[376] = UNK_0x5545C218(4);
-Global_34581[377] = UNK_0x5545C218(5);
-Global_34581[378] = UNK_0x5545C218(6);
-Global_34581[379] = UNK_0xE623B382(1);
-Global_34581[380] = UNK_0xE623B382(2);
-Global_34581[381] = UNK_0x5545C218(7);
-Global_34581[382] = UNK_0x5545C218(8);
-Global_34581[383] = UNK_0x5545C218(9);
-Global_34581[385] = UNK_0xE623B382(0);
-Global_34581[387] = UNK_0x5545C218(10);
-Global_34581[388] = UNK_0x5545C218(11);
-Global_34581[394] = UNK_0x5545C218(12);
-Global_34581[44] = UNK_0xE623B382(3);
-Global_34581[392] = UNK_0xE623B382(4);
-Global_34581[391] = UNK_0xE623B382(6);
-Global_34581[393] = UNK_0xE623B382(5);
-Global_34581[395] = UNK_0xE623B382(7);
